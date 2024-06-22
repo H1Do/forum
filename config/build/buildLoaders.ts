ed40c6d7da9366ba8bf4c9b3f8c7de1cecdd43b7
Niyaz Latypov <hidouchiha@gmail.com>
2024-06-22 14:32:42 +0500

    feat: added i18n, babel libs and HMD, svgr, css modules and scss plugins. project refactored by FSD. implemented several features such as theme and lang switchers etc.

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -28,7 +28,44 @@ export function buildLoader({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     };
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
+
     return [
+        svgLoader,
+        fileLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ]
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,28 @@
+import {FC, useState} from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Sidebar.module.scss';
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwitcher";
+
+interface SidebarProps { 
+    className?: string;
+}
+
+export const Sidebar: FC<SidebarProps> = ({className}) => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const onToggle = () => {
+        setCollapsed(collapsed => !collapsed);
+    }
+
+    return (
+        <div className={classNames(cls.sidebar, {[cls.collapsed]: collapsed}, [className])}>
+            <button onClick={onToggle}>toggle</button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+                <LangSwitcher />
+            </div>
+        </div>
+    );
+};
+ 
